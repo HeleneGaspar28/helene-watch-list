7f63d6ec2bed9e8067eb90f19f4bef137adc7b1a
Helene Gaspar <helene.gaspar@velopass.com>
2025-06-20 10:37:21 +0200

    updated rails app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should ensure the existence of records required to run the application in every environment (production,
 # development, test). The code here should be idempotent so that it can be executed at any point in every environment.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
@@ -7,3 +8,49 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning up database..."
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts "Creating movies..."
+movie1 = Movie.create!(
+  title: "Wonder Woman 1984",
+  overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s",
+  poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
+  rating: 6.9
+)
+
+movie2 = Movie.create!(
+  title: "The Shawshank Redemption",
+  overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison",
+  poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+  rating: 8.7
+)
+
+movie3 = Movie.create!(
+  title: "Titanic",
+  overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.",
+  poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg",
+  rating: 7.9
+)
+
+movie4 = Movie.create!(
+  title: "Ocean's Eight",
+  overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.",
+  poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg",
+  rating: 7.0
+)
+
+puts "Creating lists..."
+action_list = List.create!(name: "Action")
+drama_list = List.create!(name: "Drama")
+girl_power_list = List.create!(name: "Girl Power")
+
+puts "Creating bookmarks..."
+Bookmark.create!(movie: movie1, list: girl_power_list, comment: "Strong female lead")
+Bookmark.create!(movie: movie2, list: drama_list, comment: "Classic prison escape")
+Bookmark.create!(movie: movie3, list: drama_list, comment: "Epic love story")
+Bookmark.create!(movie: movie4, list: girl_power_list, comment: "All-female heist team")
+
+puts "Seeding complete."
